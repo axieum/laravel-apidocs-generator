0865f44924484a4d4e4e1e43d1c377de9cd12dfb
Axieum <7577851+Axieum@users.noreply.github.com>
2020-03-29 13:28:34 +1000

    Prettify markdown content

--- src/formatter/MarkdownFormatter.php
+++ src/formatter/MarkdownFormatter.php
@@ -2,6 +2,9 @@
 
 namespace Axieum\ApiDocs\formatter;
 
+/**
+ * Documentation Formatter for cleaning up Blade rendered Markdown.
+ */
 class MarkdownFormatter implements DocFormatter
 {
     /**
@@ -9,6 +12,21 @@ class MarkdownFormatter implements DocFormatter
      */
     public static function format(string $content): string
     {
-        return $content;
+        // NB: Blade comments, includes and components all result in
+        //     inconsistent line breaks.
+
+        // Add line breaks before titles
+        $markdown = preg_replace('/(#+.*)/', "\n$1", $content);
+
+        // Remove line breaks after titles
+        $markdown = preg_replace('/(#+.*)\n+/', "$1\n", $markdown);
+
+        // Remove excessive line breaks
+        $markdown = preg_replace('/^\n+/m', "\n", $markdown);
+
+        // Trim the content, and end with a blank line
+        $markdown = trim($markdown) . "\n";
+
+        return $markdown;
     }
 }
